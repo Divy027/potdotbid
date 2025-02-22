5da7b68d3dcf0a6ad9547c76e034a83cae74b2fd
div-lync <divy@lync.world>
2025-02-22 17:13:39 +0530

    sepolia

--- config/index.ts
+++ config/index.ts
@@ -4,11 +4,11 @@ import tokenabi from "./ABI/token.json"
 
 export const backend_url = process.env.NEXT_PUBLIC_BACKEND_URL
 export const BondingCurve = {
-    contractAddress: '0xA83B86519fEfd3E0Cbd617C1B9850842d14Ad779',
+    contractAddress: '0xDFeAAB3b097a8c00C3a886f8D57Da5ba6D711ab5',
     ABI: ABI,
 }
 export const ERC20ABI = ercABI;
 export const TokenABI = tokenabi;
 
 export const indexer = "https://indexer.dev.hyperindex.xyz/36b50e6/v1/graphql"
-export const RPC = "https://sepolia.base.org"
+export const RPC = "https://eth-sepolia.public.blastapi.io"
